c58eb69b963d8031246313f484fa98438f409dff
Nils Heinig <nils.heinig@uni-oldenburg.de>
2018-08-06 19:31:33 +0200

    erste dummy regel implementiert

--- src/InferenceControll.java
+++ src/InferenceControll.java
@@ -6,47 +6,84 @@ public class InferenceControll {
 	static ArrayList<Parameter> listOfPersonValues = new ArrayList<Parameter>();
 	static ArrayList<Parameter> listOfSpO2Values = new ArrayList<Parameter>();
 	static String alarm = "None";
-	
-	
-	
+
+	// helper method that prints out the lists of parameter values
 	static void printValueLists() {
-		for(int i = 0; i < listOfPersonValues.size(); i++) {
+		for (int i = 0; i < listOfPersonValues.size(); i++) {
 			Parameter param;
 			param = listOfPersonValues.get(i);
 			System.out.println(param.parameterType + " " + param.parameterValue + " " + param.timestamp);
 		}
-		for(int j = 0; j < listOfSpO2Values.size(); j++) {
-		Parameter param;
-		param = listOfSpO2Values.get(j);
-		System.out.println(param.parameterType + " " + param.parameterValue + " " + param.timestamp);
+		for (int j = 0; j < listOfSpO2Values.size(); j++) {
+			Parameter param;
+			param = listOfSpO2Values.get(j);
+			System.out.println(param.parameterType + " " + param.parameterValue + " " + param.timestamp);
 		}
-		
+
 	}
-	
+
+	// method that handles new incoming Parameters and writes them in their specific
+	// Arraylist<Parameter>
 	static void handleNewParameterValue(Parameter param) {
 		if (param.parameterType.equals("persons")) {
 			listOfPersonValues.add(param);
-		} else if(param.parameterType.equals("spo2")) {
+		} else if (param.parameterType.equals("spo2")) {
 			listOfSpO2Values.add(param);
 		}
 	}
-	
-	static boolean registerRule(List<String> rule) {
+
+	// method that registers a Rule to lostOfRules
+	static boolean registerRule(ArrayList<String> rule) {
 		if (!listOfRules.contains(rule)) {
 			listOfRules.add(rule);
+			System.out.println(listOfRules);
 			return true;
 		} else {
 			return false;
 		}
-		
+
 	}
-	
-	
+
+	// set the alarm type
+	static void setAlarm(String alarmtype) {
+		alarm = alarmtype;
+	}
+
 	public static void main(String[] args) {
+		// register Rule for people and spo2 value at IC
+		PersonSpO2Rule pspo2 = new PersonSpO2Rule("pspo2");
+		System.out.println(pspo2.listOfParametersNeeded);
+
+		// get dummy Parameters
 		InputDummy id = new InputDummy();
-		
 		id.fillList();
 		printValueLists();
+
+		while (!listOfPersonValues.isEmpty() && !listOfSpO2Values.isEmpty()) {
+			List<String> tempList = listOfRules.get(0);
+			System.out.println(tempList);
+
+			if (tempList.contains("persons")) {
+				pspo2.setPersons(listOfPersonValues.get(0));
+			}
+			if (tempList.contains("spo2")) {
+				pspo2.setSpO2(listOfSpO2Values.get(0));
+			}
+			System.out.println(
+					pspo2.persons.parameterType + " " + pspo2.persons.parameterValue + " " + pspo2.persons.timestamp);
+			System.out.println(pspo2.spo2.parameterType + " " + pspo2.spo2.parameterValue + " " + pspo2.spo2.timestamp);
+
+			if (pspo2.ruleLogic()) {
+				pspo2.ruleAction();
+			} else {
+				setAlarm("None!");
+			}
+			System.out.println(alarm);
+			
+			listOfPersonValues.remove(0);
+			listOfSpO2Values.remove(0);
+		}
+
 	}
 
 }
--- src/PersonSpO2Rule.java
+++ src/PersonSpO2Rule.java
@@ -2,31 +2,51 @@ import java.util.ArrayList;
 
 public class PersonSpO2Rule extends Rule {
 
-	PersonSpO2Rule(String ruleName, ArrayList<String> parameterList) {
-		super(ruleName, parameterList);
-		// TODO Auto-generated constructor stub
+	Parameter persons;
+	Parameter spo2;
+	
+	PersonSpO2Rule(String ruleName) {
+		super(ruleName);
+		initializeRule();
 	}
 
 	// method to decide if ruleAction has to be activated
 	boolean ruleLogic() {
-		return false;
+		if (spo2.parameterValue.equals("low")) {			
+			return true;
+		}else {
+			return false;
+		}
 	}
 	
 	//method that evaluates Parameters and forwards the result to the InferenceControll
 	void ruleAction() {
+		if(persons.parameterValue.equals("none") && spo2.parameterValue.equals("low")) {
+			InferenceControll.setAlarm("HNR ALARM!");
+		} else if (persons.parameterValue.equals("many") && spo2.parameterValue.equals("low"))
+			InferenceControll.setAlarm("LOCAL ALARM!");
+		
 	}
 	
 	//method to get Parameters if they are missing
 	void getParameters() {
 	}
 	
-	//method that initializes the rule by registering it to the InferenceControll and adding the parameters needed to the list of parameters
-	void initializeRule() {
-
-		//registerRuleAtInferenceControll();
-		
+	
+	void setPersons(Parameter persons) {
+		this.persons = persons;
 	}
 	
+	void setSpO2(Parameter spo2) {
+		this.spo2 = spo2;
+	}
 	
+	//method that initializes the rule by registering it to the InferenceControll and adding the parameters needed to the list of parameters
+	void initializeRule() {
+		this.listOfParametersNeeded.add(ruleName);
+		this.listOfParametersNeeded.add("persons");
+		this.listOfParametersNeeded.add("spo2");
+		this.registerRuleAtInferenceControll();		
+	}
 		
 }
