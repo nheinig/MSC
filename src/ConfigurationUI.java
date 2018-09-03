6640835fa6e58c66e9bd532fee4569fdd4e00502
Nils Heinig <nils.heinig@uni-oldenburg.de>
2018-09-03 11:24:40 +0200

    first step to create a rule

--- src/ConfigurationUI.java
+++ src/ConfigurationUI.java
@@ -1,5 +1,18 @@
-import java.util.ArrayList;
+import java.util.*;
 import javax.swing.*;
+
+import java.awt.event.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+ 
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.StandardLocation;
+import javax.tools.ToolProvider;
 
 public class ConfigurationUI {
 
@@ -48,6 +61,39 @@ public class ConfigurationUI {
 		}
 	}
 	
+	void createNewRule() throws Exception{
+		// create an empty source file
+        File sourceFile = File.createTempFile("Hello", ".java");
+        sourceFile.deleteOnExit();
+ 
+        // generate the source code, using the source filename as the class name
+        String classname = sourceFile.getName().split("\\.")[0];
+        String sourceCode = "public class " + classname + "{ public void hello() { System.out.print(\"Hello world\");}}";
+ 
+        // write the source code into the source file
+        FileWriter writer = new FileWriter(sourceFile);
+        writer.write(sourceCode);
+        writer.close();
+        /*
+        // compile the source file
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
+        File parentDirectory = sourceFile.getParentFile();
+        fileManager.setLocation(StandardLocation.CLASS_OUTPUT, Arrays.asList(parentDirectory));
+        Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromFiles(Arrays.asList(sourceFile));
+        compiler.getTask(null, fileManager, null, null, null, compilationUnits).call();
+        fileManager.close();
+        
+        // load the compiled class
+        URLClassLoader classLoader = URLClassLoader.newInstance(new URL[] { parentDirectory.toURI().toURL() });
+        Class<?> helloClass = classLoader.loadClass(classname);
+        
+        // call a method on the loaded class
+        Method helloMethod = helloClass.getDeclaredMethod("hello");
+        helloMethod.invoke(helloClass.newInstance());
+*/
+	}
+	
 	//Method to create the Configuration UI
 	public void createConfigurationUI() {		
 		JFrame ui = new JFrame("Inference Engine");
@@ -69,6 +115,18 @@ public class ConfigurationUI {
 		box.add(paramBox2);
 		
 		box.add(finishButton);
+		finishButton.addActionListener(new ActionListener() {
+
+		    public void actionPerformed(ActionEvent e) {
+		        try {
+					createNewRule();
+					System.out.println("Rule was " + ruleNameTF.getText() + " created!");
+				} catch (Exception e1) {
+					System.out.println("Rule could not be created!");
+					e1.printStackTrace();
+				}
+		    }
+		});
 
 		ui.add(box);
 			
